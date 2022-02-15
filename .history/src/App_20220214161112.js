ebe25fca77f2ab13bb8644b1762fc7ef43c19a2f
Aishwaryapandey123 <aishwarya@gmail.com>
2022-02-14 16:22:04 -1200

    added react-router

--- .history/src/App_20220214161112.js
+++ .history/src/App_20220214161112.js
@@ -4,11 +4,7 @@ import { Switch, Route} from 'react-router-dom';
 function App() {
   return (
   <Switch>
-     
-     <Route exact  path= "/"> This is home page. </Route>
-
-     <Route exact  path= "/starred"> 
-     This is starred. </Route>
+      <Route path= "/"> This is home page. </Route>
     </Switch>
   );
 }
