b16159647fa448bccfef778a4d6ab2a890d14df6
Aishwaryapandey123 <aishwarya@gmail.com>
2022-02-19 01:50:41 -1200

    styled show page

--- .history/src/components/show/ShowMainData_20220219014613.js
+++ .history/src/components/show/ShowMainData_20220219014613.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 import IMG_PLACEHOLDER from '../../images/not-found.png';
 import { Star } from '../styled';
-import { MainDataWrapper, Headline,TagList } from './ShowMainData.styled';
+import { MainDataWrapper, Headline,Taglist } from './ShowMainData.styled';
 
 const ShowMainData = ({ name, rating, summary, tags, image }) => {
   return (
@@ -22,11 +22,11 @@ const ShowMainData = ({ name, rating, summary, tags, image }) => {
 
         <div>
           Tags:{' '}
-          <TagList>
+          <Taglist>
             {tags.map((tag, i) => (
               <span key={i}>{tag}</span>
             ))}
-          </TagList>
+          </Taglist>
         </div>
       </div>
     </MainDataWrapper>
---
+++ .history/src/pages/starred_20220219014555.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+import MainpageLayout from '../components/MainpageLayout';
+
+const Starred = () => {
+  return <MainpageLayout> This is Starred</MainpageLayout>;
+};
+
+export default Starred;
